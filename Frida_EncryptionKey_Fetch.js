e912cab12fcf28b66ceb562fd1bd494b8826955a
Muhammad Talha Khan <MTK911@users.noreply.github.com>
2022-09-14 13:30:09 +0500

    Create Frida_EncryptionKey_Fetch.js

---
+++ Frida_EncryptionKey_Fetch.js
@@ -0,0 +1,5 @@
+var secret_key_spec = Java.use("javax.crypto.spec.SecretKeySpec");
+    secret_key_spec.$init.overload("[B", "java.lang.String").implementation = function (x, y) {
+        send('{"my_type" : "KEY"}', new Uint8Array(x));
+        return this.$init(x, y);
+    }
